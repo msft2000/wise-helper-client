b207863782abff176a73a30219650fb261c13e4b
Renato Berrezueta <renatoberrezueta23@gmail.com>
2023-05-28 23:52:00 -0500

    inicio de footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+function Footer(){
+    navigate=useNavigate();//Para navegar entre paginas del sitio
+
+    return(
+        <footer>
+
+
+
+        </footer>
+    );
+}
