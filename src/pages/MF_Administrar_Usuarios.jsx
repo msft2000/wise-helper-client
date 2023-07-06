0c958d19f0a1881b7050565adcf1a89c06a82fc3
Mateo Flores <dragonnegro1231@gmail.com>
2023-07-05 21:55:42 -0500

    Cambios previos a recivir version del renato

--- src/pages/MF_Administrar_Usuarios.jsx
+++ src/pages/MF_Administrar_Usuarios.jsx
@@ -1,16 +1,64 @@
 import React from "react";
+import ax from "axios";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header_Admin";
+import toast, { Toaster } from "react-hot-toast";
 import "../sass/mf_administrar_usuarios.scss";
 
 function AdministrarUsuarios() {
+  const [users, setUsers] = React.useState([]);
+
+  const getUsers = async () => {
+    const config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: "https://wise-helper-backend.onrender.com/api/v1/auth/all",
+      headers: {},
+      data: "",
+    };
+    ax.request(config)
+      .then((response) => {
+        setUsers(response.data.users);
+      })
+      .catch((error) => {
+        toast.error("Error al obtener los usuarios");
+      });
+  };
+  React.useEffect(() => {
+    getUsers();
+  }, []);
   return (
     <React.Fragment>
       <Header />
       <div id="administrar-usuarios">
-        <h1>Administrar Usuarios</h1>
+        <h1>Lista de Usuarios</h1>
+        <div className="users--container">
+          {Object.keys(users).length > 0 ? (
+            users.map((user) => {
+              return (
+                <div className="user--card" hey={user._id}>
+                  <div className="user--card__info">
+                    <h2>{user.name}</h2>
+                    <p>{user.email}</p>
+                  </div>
+                  <div className="user--card__buttons">
+                    <button className="btn--edit">Editar</button>
+                    <button className="btn--delete">Eliminar</button>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <div className="user--card">
+              <div className="user--card__info">
+                <h2>No hay usuarios</h2>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
       <Footer />
+      <Toaster />
     </React.Fragment>
   );
 }
